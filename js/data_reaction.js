8983c16cff6aab04d190d6e2dbb06fc93c22aff9
Karbb <andrea.rubino1990@gmail.com>
2020-11-22 14:14:54 +0100

    Update data_reaction.js

--- js/data_reaction.js
+++ js/data_reaction.js
@@ -1,19 +1,19 @@
 data_reaction = [
     {
-        title: "Opportunity attack",
+        title: "Attacco d'opportunità",
         icon: "crossed-swords",
-        subtitle: "Enemy leaves your reach",
-        description: "You can rarely move heedlessly past your foes without putting yourself in danger",
+        subtitle: "Nemico esce dalla tua portata",
+        description: "Raramente puoi muoverti senza pericolo vicino i tuoi nemici senza metterti in pericolo",
         reference: "PHB, pg. 195.",
         bullets: [
-            "Trigger: enemy creature you can see leaves your reach.",
-            "Make one melee attack against the provoking creature.",
-            "The attack interrupts the provoking creature's movement, occurring right before the creature leaves your reach.",
+            "Condizione: una creatura nemica visibile esce dalla tua portata.",
+            "Effettua un attacco corpo a corpo contro la creatura che l'ha provocato.",
+            "L'attacco interrompe il movimento della creatura che l'ha provocato e si verifica appena prima che la creatura esca dalla tua portata.",
             "Creatures don't provoke an opportunity attack when they teleport or when someone or something moves them without using their movement, action, or reaction."
         ]
     },
     {
-        title: "Readied action",
+        title: "Azione Preparata",
         icon: "stopwatch",
         subtitle: "Part of your Ready action",
         description: "Execute the reaction specified by your Ready action",
@@ -23,9 +23,9 @@ data_reaction = [
         ]
     },
     {
-        title: "Cast a spell",
+        title: "Lancia un'incantesimo",
         icon: "magic-swirl",
-        subtitle: "Cast time of 1 reaction",
+        subtitle: "Tempo di lancio di 1 reazione",
         description: "Cast a spell with a casting time of 1 reaction",
         reference: "PHB, pg. 192.",
         bullets: [
