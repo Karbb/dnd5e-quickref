e7699ad30598af444cd214ac7fd668d515d5d046
Karbb <andrea.rubino1990@gmail.com>
2020-11-22 12:37:30 +0100

    Update data_bonusaction.js

--- js/data_bonusaction.js
+++ js/data_bonusaction.js
@@ -1,21 +1,21 @@
 data_bonusaction = [
     {
-        title: "Offhand Attack",
+        title: "Attacco con la mano secondaria",
         icon: "crossed-swords",
-        subtitle: "Use with the Attack action",
+        subtitle: "Usata insieme ad un'azione d'attacco",
         description: "Attack with your off hand",
         reference: "PHB, pgs. 192,194-195.",
         bullets: [
-            "Only usable if you take the <i>Attack</i> action and attack with a light melee weapon that you're holding in one hand.",
+            "Usabile solo se hai effettuato un azione <i>Attacco</i> e attacchi con un'arma da mischia leggera che stai impugnando in una sola mano.",
             "Perform a single attack with a different light melee weapon that you're holding in the other hand.",
             "You don't add your ability modifier to the damage of the bonus attack, unless that modifier is negative.",
             "If either weapon has the thrown property, you can throw the weapon, instead of making a melee attack with it."
         ]
     },
     {
-        title: "Cast a spell",
+        title: "Lancio di un incantesimo",
         icon: "magic-swirl",
-        subtitle: "Cast time of 1 bonus action",
+        subtitle: "Tempo di lancio di un'azione bonus",
         description: "Cast a spell with a casting time of 1 bonus action",
         reference: "PHB, pg. 192.",
         bullets: [
@@ -24,9 +24,9 @@ data_bonusaction = [
         ]
     },
     {
-        title: "Use class feature",
+        title: "Usare un talento di classe",
         icon: "embrassed-energy",
-        subtitle: "Some features use bonus actions",
+        subtitle: "Alcuni talenti usano azioni bonus",
         description: "Use a racial or class feature that uses a bonus action",
         reference: "See class page for more information.",
         bullets: [
